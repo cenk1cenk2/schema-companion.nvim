03e7ce19290e4c96e751ef64584e2c15754bd8c4
Marius Svechla <m.svechla@gmail.com>
2024-03-27 20:49:29 +0100

    feat: automatically detect Kubernetes CRD schemas

--- lua/yaml-companion/config.lua
+++ lua/yaml-companion/config.lua
@@ -5,66 +5,66 @@ local add_hook_after = require("lspconfig.util").add_hook_after
 
 ---@type ConfigOptions
 M.defaults = {
-  log_level = "info",
-  formatting = true,
-  builtin_matchers = {
-    kubernetes = { enabled = true },
-    cloud_init = { enabled = true },
-  },
-  schemas = {},
-  lspconfig = {
-    flags = {
-      debounce_text_changes = 150,
+    log_level = "info",
+    formatting = true,
+    builtin_matchers = {
+        kubernetes_crd = { enabled = true },
+        cloud_init = { enabled = true },
     },
-    single_file_support = true,
-    settings = {
-      redhat = { telemetry = { enabled = false } },
-      yaml = {
-        validate = true,
-        format = { enable = true },
-        hover = true,
-        schemaStore = {
-          enable = true,
-          url = "https://www.schemastore.org/api/json/catalog.json",
+    schemas = {},
+    lspconfig = {
+        flags = {
+            debounce_text_changes = 150,
         },
-        schemaDownload = { enable = true },
-        schemas = { result = {} },
-        trace = { server = "debug" },
-      },
+        single_file_support = true,
+        settings = {
+            redhat = { telemetry = { enabled = false } },
+            yaml = {
+                validate = true,
+                format = { enable = true },
+                hover = true,
+                schemaStore = {
+                    enable = true,
+                    url = "https://www.schemastore.org/api/json/catalog.json",
+                },
+                schemaDownload = { enable = true },
+                schemas = { result = {} },
+                trace = { server = "debug" },
+            },
+        },
     },
-  },
 }
 
 ---@type ConfigOptions
 M.options = {}
 
 function M.setup(options, on_attach)
-  if options == nil then
-    options = {}
-  end
-
-  if options.lspconfig == nil then
-    options.lspconfig = {}
-  end
-
-  M.options = vim.tbl_deep_extend("force", {}, M.defaults, options or {})
-
-  M.options.lspconfig.on_attach = add_hook_after(options.lspconfig.on_attach, on_attach)
-
-  M.options.lspconfig.on_init = add_hook_after(options.lspconfig.on_init, function(client)
-    client.notify("yaml/supportSchemaSelection", { {} })
-    return true
-  end)
-
-  for name, matcher in pairs(M.options.builtin_matchers) do
-    if matcher.enabled then
-      matchers.load(name)
+    if options == nil then
+        options = {}
     end
-  end
 
-  handlers["yaml/schema/store/initialized"] =
-    require("yaml-companion.lsp.handler").store_initialized
-  M.options.lspconfig.handlers = handlers
+    if options.lspconfig == nil then
+        options.lspconfig = {}
+    end
+
+    M.options = vim.tbl_deep_extend("force", {}, M.defaults, options or {})
+
+    M.options.lspconfig.on_attach = add_hook_after(options.lspconfig.on_attach, on_attach)
+
+    M.options.lspconfig.on_init = add_hook_after(options.lspconfig.on_init, function(client)
+        client.notify("yaml/supportSchemaSelection", { {} })
+        return true
+    end)
+
+    for name, matcher in pairs(M.options.builtin_matchers) do
+        if matcher.enabled then
+            matchers.load(name)
+        end
+    end
+
+    handlers["yaml/schema/store/initialized"] =
+        require("yaml-companion.lsp.handler").store_initialized
+    M.options.lspconfig.handlers = handlers
 end
 
 return M
