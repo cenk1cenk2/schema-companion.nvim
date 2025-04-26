aa6a1cea10b2c91c54d45e2abb8749b8ff9b7ba4
Cenk Kılıç <cenk@kilic.dev>
2025-04-26 23:01:54 +0200

    fix: update call

--- lua/schema-companion/context.lua
+++ lua/schema-companion/context.lua
@@ -115,9 +115,7 @@ function M.schema(bufnr, data)
     log.debug("set new override: file=%s schema=%s", bufuri, data.uri)
 
     client.settings = vim.tbl_deep_extend("force", client.settings, { yaml = { schemas = override } })
-    -- client.request("workspace/didChangeConfiguration", { settings = client.settings })
-
-    client.workspace_did_change_configuration(client.settings)
+    client.notify("workspace/didChangeConfiguration", { settings = client.settings })
   end
 
   return M.ctx[bufnr].schema
